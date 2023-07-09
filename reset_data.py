f8a47cc3b5e9afd1c4abc1f3f363467fea940966
tiny-mathematics <138893855+tiny-mathematics@users.noreply.github.com>
2023-07-09 09:29:38 -0400

    Update reset_data.py

--- reset_data.py
+++ reset_data.py
@@ -6,18 +6,16 @@ from dateutil.parser import parse
 import pandas as pd
 import numpy as np
 
-from itertools import combinations
-
 import datetime
-
-def fetch_player_data():
-    player_data = pd.read_csv('data/player_data.csv')
-    return player_data
 
 def fetch_constants():
     with open('data/constants.json', 'r') as file:
         constants = json.load(file)
     return constants
+    
+def fetch_player_data():
+    player_data = pd.read_csv('data/player_data.csv')
+    return player_data
 
 def fetch_player_game_data():
     player_game_data = pd.read_csv('data/player_game_data.csv')
@@ -49,24 +47,38 @@ def update_google_sheets():
     data_list = [metadata.columns.values.tolist()] + metadata.values.tolist()
     gsheet.worksheet(metadata_sheet_name).append_rows(data_list)
 
+def reset_data:
+    global player_data, variant_data, player_game_data, constants
+
+    player_game_data = pd.DataFrame(columns=player_game_data.columns)
+    player_game_data.to_csv('data/player_game_data.csv', index=False)
+
+    player_data['player_rating'] = 1400
+    player_data['top_streak'] = 0
+    player_data['current_streak'] = 0
+    player_data['number_of_games'] = 0
+    player_data['number_of_max_scores'] = 0
+    player_data.to_csv('data/player_data.csv', index=False)
+
+    variant_data['variant_rating'] = variant_data['variant_name'].map(variant_base_ratings)
+    variant_data['number_of_games_variant'] = 0
+    variant_data['number_of_max_scores_variant'] = 0
+    variant_data.to_csv('data/variant_data.csv', index=False)
+
+    constants['latest_game_id'] = constants['starting_game_id'] - 1
+    constants['total_games_played'] = 0
+    constants['latest_run'] = None
+    with open('data/constants.json, 'w') as file:
+        json.dump(constants, file)
 
 
 def main():
-    player_data = fetch_player_data()
     constants = fetch_constants()
+    player_data = fetch_player_data()
     player_game_data = fetch_player_game_data()
     variant_data = fetch_variant_data()
-    variants = build_variant_list()
-    game_data = fetch_game_data()
-    
-    if game_data is not None and not game_data.empty:
-        print("Calculating ratings")
-        calculate_ratings()
-        print("Updating Google Sheets")
-        update_google_sheets()
-        print("Done!")
-    else:
-        print('no new games')
+
+    reset_data()
 
 if __name__ == "__main__":
     main()
